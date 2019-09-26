a47da6a3e77f100ab620348e461782b6ddb2f128
MagicMike0 <mike.malecot@yahoo.fr>
2019-09-26 17:23:57 +0200

    testOverlapping

---
+++ src/interval.test.js
@@ -0,0 +1,16 @@
+const Interval = require('./interval');
+
+describe('overlaps', function () {
+
+	test('Test overlapping, if two intervals overlap each other', () => {
+		let interval1 = new Interval(1, 3);
+		let interval2 = new Interval(2, 4);
+    	expect(interval1.overlaps(interval2)).toBe(true);
+	});
+
+	test('Test overlapping, if two intervals don\'t overlap each other', () => {
+		let interval1 = new Interval(1,2);
+		let interval2 = new Interval(3,4);
+		expect(interval1.overlaps(interval2)).toBe(false);
+	});
+});
