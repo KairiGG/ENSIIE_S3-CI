67e9b3aa0893776bf9c9710921de503fba0d5216
MagicMike0 <mike.malecot@yahoo.fr>
2019-09-26 15:25:03 +0200

    first commit

---
+++ src/math.js
@@ -0,0 +1,115 @@
+let Util = {};
+Util.factorial = (n) => {
+    if (n === 0) {
+        return 1;
+    }
+
+    if (n >= 3000) {
+        throw 'n too large'
+    }
+
+    if (n < 0) {
+        throw 'n is negative'
+    }
+
+    return n * Util.factorial(n - 1);
+};
+
+/**
+ * Détermine si n est un nombre premier.
+ * Util.isPrime(5) => false
+ * Util.isPrime(6) => true
+ *
+ * @param {number} n
+ * @returns {boolean}
+ */
+Util.isPrime = function (n) {
+    if (n === 1 || n === 0) {
+        return false;
+    }
+    if (n < 0) {
+        throw 'Unable to compute prime for n < 0'
+    }
+    for (var i = 2; i < n; i++)
+        if (n % i === 0) return false;
+    return true;
+
+};
+
+
+/**
+ * Additionne l'ensemble des nombres premiers de 2 à n
+ *
+ * Util.sumPrime(6) = 2 + 3 + 5 = 10
+ * Util.sumPrime(8) = 2 + 3 + 5 + 7 = 17
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+Util.sumPrime = function(n) {
+	if(!Util.isPrime(n)) {
+		throw 'n is not prime'
+	}
+    else {
+    	let result = 0;
+    	for(let i = 2; i <= n; i++) {
+    		if(Util.isPrime(i))
+    			result += i;
+    	}
+    	return result;
+    }	
+};
+
+/**
+ * Cette méthode doit retourner un tableau de 1 à n tel que:
+ * - Pour les nombres multiples de 3, les remplacer par "Fizz"
+ * - Pour les nombres multiples de 5, les remplacer par "Buzz"
+ * - Pour les nombres multiples de 3 et 5, les remplacer par "FizzBuzz"
+ *
+ * Exp :
+ * Util.fizzBuzz(15) => [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+ *
+ * @param {number} n
+ * @returns {array}
+ */
+Util.fizzBuzz = function(n) {
+	if(n < 1)
+		throw 'n should be superior to 0'
+	let result = []
+	for(let i=1; i < n + 1; i++)
+		result.push((i%3?'':'Fizz') + (i%5?'':'Buzz') || i)
+	return result;
+};
+
+/**
+ * Chiffre une phrase selon la règle suivante : Les A deviennent des B, les B des C, etc.
+ *
+ * Exp :
+ * Util.cipher("Test Unitaire") => "Uftu Tojubjsf"
+ *
+ * @param phrase
+ * @returns {string}
+ */
+Util.cipher = function (phrase) {
+	let charTransformation = (char) => {
+		let charValue = char.charCodeAt(0);
+		if((charValue < 65 || charValue > 122) 
+		&& (charValue > 90 || charValue < 97)) {
+			return char; //special char -> return char
+		} else {
+			if(charValue == 90) {
+				return 'A'; //'Z' case -> return 'A'
+			} else if(charValue == 122) {
+				return 'a'; //'z' case -> return 'a'
+			} else {
+				return String.fromCharCode(charValue + 1);
+			}
+		}
+	};
+
+	let charCollection = phrase.split("");
+	return charCollection.map( c => charTransformation(c) )
+			.reduce( (str, char) => str += char );
+};
+
+module.exports = Util;
---
+++ src/math.test.js
@@ -0,0 +1,109 @@
+const Util = require('./math');
+
+describe('isPrime', function () {
+
+	test('Test factoriel de 0 => 1', () => {
+    	expect(Util.factorial(0)).toBe(1);
+	});
+
+	test('Test factoriel de 2 => 2', () => {
+		expect(Util.factorial(3)).toBe(6);
+	});
+
+	test('Test factoriel de 3 => 6', () => {
+		expect(Util.factorial(3)).toBe(6);
+	});
+
+	test('Test factoriel de 3000', () => {
+		expect(()=> {Util.factorial(3000)}).toThrow();
+	});
+
+	test('Test factoriel -10', () => {
+		expect(()=> {Util.factorial(-10)}).toThrow(/negative/);
+	});
+});
+
+
+
+describe('isPrime', function () {
+
+    test('Test prime de 1 => false', () => {
+        expect(Util.isPrime(1)).toBe(false)
+    });
+    test('Test prime de 0 => false', () => {
+        expect(Util.isPrime(0)).toBe(false)
+    });
+    test('Test prime < 0 => throw exception', () => {
+        expect(() => { Util.isPrime(-10) }).toThrow('Unable to compute prime for n < 0');
+    });
+
+    test.each([
+        [2, true],
+        [5, true],
+        [17, true],
+        [18, false],
+        [53, true],
+        [55, false],
+    ])(
+        'isPrime %i equals to %i',
+        (n, expected) => {
+            expect(Util.isPrime(n)).toBe(expected);
+        }
+    );
+});
+
+describe('sumPrime', function () {
+
+	test('Test somme d\'un nombre non-premier négatif, n=-10', () => {
+    	expect(() => {Util.sumPrime(-10)}).toThrow('Unable to compute prime for n < 0');
+	});
+
+	test('Test somme d\'un nombre non-premier, n=26', () => {
+    	expect(() => {Util.sumPrime(26)}).toThrow('n is not prime');
+	});
+
+	test('Test somme d\'un nombre premier, n=7', () => {
+    	expect(Util.sumPrime(7)).toBe(17);
+	});
+
+	test('Test somme d\'un nombre premier, n=47', () => {
+    	expect(Util.sumPrime(47)).toBe(328);
+	});
+});
+
+describe('fizzBuzz', function () {
+
+	test('Test FizzBuzz n < 1, n=0', () => {
+    	expect(() => {Util.fizzBuzz(0)}).toThrow('n should be superior to 0');
+	});
+
+	test('Test FizzBuzz n < 1, n=-63', () => {
+    	expect(() => {Util.fizzBuzz(-63)}).toThrow('n should be superior to 0');
+	});
+
+	test('Test FizzBuzz, n=15', () => {
+		let expectedResult = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"];
+    	expect(Util.fizzBuzz(15)).toStrictEqual(expectedResult);
+	});
+
+	test('Test FizzBuzz, n=30', () => {
+		let expectedResult = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"
+							, 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz"];
+    	expect(Util.fizzBuzz(30)).toStrictEqual(expectedResult);
+	});
+});
+
+describe('cipher', function () {
+	test('Test cipher, phrase="Test Unitaire"', () => {
+		expect(Util.cipher('Test Unitaire')).toBe('Uftu Vojubjsf');
+	});
+
+	test('Test cipher avec phrase contenant "z", phrase="Test Unitaire du z"', () => {
+		expect(Util.cipher('Test Unitaire du z')).toBe('Uftu Vojubjsf ev a');
+	});
+
+	test('Test cipher avec phrase contenant "Z", phrase="Test Unitaire du Z"', () => {
+		expect(Util.cipher('Test Unitaire du Z')).toBe('Uftu Vojubjsf ev A');
+	});
+
+});
